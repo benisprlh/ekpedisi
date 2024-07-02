aac743e1ea614249dbfddc8efc7dd990e4fae754
unknown <benisaprulah5@gmail.com>
2024-07-02 08:38:11 +0700

    update: update bug in association

--- Server/models/ekspedition.js
+++ Server/models/ekspedition.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Ekspedition.hasMany(models.Packages);
+      Ekspedition.hasMany(models.Package);
     }
   }
   Ekspedition.init(
