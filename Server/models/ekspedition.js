6a29a7e9ccf5aa5e0f2dcccb1a21584df95174b5
unknown <benisaprulah5@gmail.com>
2024-07-02 09:23:11 +0700

    bug: debugging foreignkey association

--- Server/models/ekspedition.js
+++ Server/models/ekspedition.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Ekspedition.hasMany(models.Package);
+      Ekspedition.hasMany(models.Package, { foreignKey: "EkspedisiID" });
     }
   }
   Ekspedition.init(
--- Server/models/package.js
+++ Server/models/package.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Package.belongsTo(models.Ekspedition);
+      Package.belongsTo(models.Ekspedition, {foreignKey: "EkspedisiID"});
     }
   }
   Package.init(
